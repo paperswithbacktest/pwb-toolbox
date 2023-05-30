d7d4193dbee9f24f2c077ede20654a475db09121
edarchimbaud <edouard.darchimbaud@gmail.com>
2023-05-30 10:33:53 +0200

    feat: add example

--- systematic_trading/helpers.py
+++ systematic_trading/helpers.py
@@ -44,3 +44,27 @@ def retry_get(
             print(f"Connection error with {url}. Retrying in {delay} seconds...")
             time.sleep(delay)
     raise ConnectionError(f"Failed to connect to {url} after {retries} retries")
+
+
+if __name__ == "__main__":
+    response = retry_get(
+        "https://api.nasdaq.com/api/analyst/A/earnings-forecast",
+        mode="curl",
+        headers={
+            "authority": "api.nasdaq.com",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+            "accept-language": "en-US,en;q=0.5",
+            "cache-control": "max-age=0",
+            "sec-ch-ua": '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "sec-gpc": "1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        },
+    )
+    print(response.json())
