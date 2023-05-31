09ee28520961863d3b48c9bad9bd06a8e048c3ac
edarchimbaud <edouard.darchimbaud@gmail.com>
2023-05-31 22:21:15 +0200

    feat: first complete implementation of the momentum strategy

--- systematic_trading/strategies/momentum.py
+++ systematic_trading/strategies/momentum.py
@@ -5,6 +5,7 @@ import os
 import backtrader as bt
 import backtrader.feeds as btfeeds
 from datasets import load_dataset
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pickle
@@ -27,20 +28,13 @@ class MomentumStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
-        print(f"{dt.isoformat()}: {txt}")
+        print(f"{dt.isoformat()},{txt}")
 
     def is_first_business_day(self, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
-        # Check if the date is a weekday (Monday to Friday)
-        if dt.weekday() < 5:
-            first_day = dt.replace(day=1)
-            # Find the first business day in the month
-            while first_day.weekday() > 4:  # Skip Saturday (5) and Sunday (6)
-                first_day += relativedelta.relativedelta(days=1)
-            # Compare the dates
-            if dt == first_day:
-                return True
-        return False
+        first_day = dt.replace(day=1)
+        first_business_day = pd.date_range(first_day, periods=1, freq="BMS")[0]
+        return dt == first_business_day.date()
 
     def next(self):
         """
@@ -48,7 +42,6 @@ class MomentumStrategy(bt.Strategy):
         """
         if not self.is_first_business_day():
             return
-        self.log(self.broker.getvalue())
 
         # Calculate returns for all stocks
         self.ret = np.array(
@@ -57,6 +50,7 @@ class MomentumStrategy(bt.Strategy):
                 for d in self.datas
             ]
         )
+        self.log(self.broker.getvalue())
 
         # Count the number of stocks that have a valid momentum signal
         num_stocks = np.count_nonzero(~np.isnan(self.ret))
@@ -69,15 +63,36 @@ class MomentumStrategy(bt.Strategy):
             if self.ret[i] > long_threshold:  # Long the top quantile stocks
                 self.order_target_percent(
                     data=d,
-                    target=1.0 / num_stocks,
+                    target=0.7 / num_stocks,
                 )
             elif self.ret[i] < short_threshold:  # Short the bottom quantile stocks
                 self.order_target_percent(
                     data=d,
-                    target=-1.0 / num_stocks,
+                    target=-0.7 / num_stocks,
                 )
             else:  # Close positions that don't meet the long or short criteria
                 self.close(data=d)
+
+
+class CashNav(bt.analyzers.Analyzer):
+    """
+    Analyzer returning cash and market values
+    """
+
+    def create_analysis(self):
+        self.rets = {}
+        self.vals = 0.0
+
+    def notify_cashvalue(self, cash, value):
+        self.vals = (
+            self.strategy.datetime.datetime(),
+            cash,
+            value,
+        )
+        self.rets[len(self)] = self.vals
+
+    def get_analysis(self):
+        return self.rets
 
 
 def main():
@@ -96,23 +111,42 @@ def main():
 
     # Create a Cerebro object
     cerebro = bt.Cerebro()
+    cerebro.broker.set_cash(1000000)
+
+    starting_date = datetime(1990, 1, 1)
 
     # Add Data Feeds to Cerebro
-    for symbol in tqdm(symbols):
-        df_symbol = df[df["symbol"] == symbol]
-        if df_symbol.date.iloc[0] > datetime(2000, 1, 1):
+    for _, symbol in enumerate(tqdm(symbols)):
+        df_symbol = df[df["symbol"] == symbol].copy()
+        if df_symbol["date"].min() > starting_date:
             continue
-        data = btfeeds.PandasData(dataname=df_symbol, datetime="date")
-        cerebro.adddata(data)
+        factor = df_symbol["adj_close"] / df_symbol["close"]
+        df_symbol["open"] = df_symbol["open"] * factor
+        df_symbol["high"] = df_symbol["high"] * factor
+        df_symbol["low"] = df_symbol["low"] * factor
+        df_symbol["close"] = df_symbol["close"] * factor
+        df_symbol.drop(["symbol", "adj_close"], axis=1, inplace=True)
+        df_symbol.set_index("date", inplace=True)
+        data = btfeeds.PandasData(dataname=df_symbol)
+        cerebro.adddata(data, name=symbol)
 
     # Add Strategy to Cerebro
     cerebro.addstrategy(
         MomentumStrategy, long_quantile=0.8, short_quantile=0.2
     )  # Adjust parameters as desired
 
+    cerebro.addanalyzer(CashNav, _name="cash_nav")
+
     # Run the Strategy
-    cerebro.run()
+    results = cerebro.run()
     print("Final Portfolio Value: %.2f" % cerebro.broker.getvalue())
+
+    dictionary = results[0].analyzers.getbyname("cash_nav").get_analysis()
+    df = pd.DataFrame(dictionary).T
+    df.columns = ["Date", "Cash", "Nav"]
+    df.set_index("Date", inplace=True)
+    df.loc[df.index >= starting_date, ["Nav"]].plot()
+    plt.show()
 
     # Plot the Results
     # cerebro.plot()
