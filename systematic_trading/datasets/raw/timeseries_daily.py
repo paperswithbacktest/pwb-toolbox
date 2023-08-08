861e81b4ede524d3ca0a4990ae87f7e5d1a70d25
edarchimbaud <edouard.darchimbaud@gmail.com>
2023-08-08 11:59:46 +0200

    feat: added new header

--- systematic_trading/datasets/raw/timeseries_daily.py
+++ systematic_trading/datasets/raw/timeseries_daily.py
@@ -12,7 +12,7 @@ import pandas as pd
 import requests
 
 from systematic_trading.datasets.raw import Raw
-from systematic_trading.helpers import retry_get
+from systematic_trading.helpers import retry_get, yahoo_headers
 
 
 class TimeseriesDaily(Raw):
@@ -35,23 +35,15 @@ class TimeseriesDaily(Raw):
         ]
         self.dataset_df = pd.DataFrame(columns=self.expected_columns)
 
-    def __get_timeseries_daily_with_retry(self, ticker: str, retries=10, delay=300):
+    def __get_timeseries_daily_with_retry(self, ticker: str, retries=10, delay=30):
         from_timestamp = int(datetime.timestamp(datetime(1980, 1, 1)))
         to_timestamp = int(datetime.timestamp(datetime.now()))
         url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_timestamp}&period2={to_timestamp}&interval=1d&events=history&includeAdjustedClose=true"
-        for _ in range(retries):
-            try:
-                response = retry_get(url)
-                data = StringIO(response.text)
-                df = pd.read_csv(data)
-                df["Date"] = pd.to_datetime(df["Date"]).dt.date.apply(
-                    lambda x: x.isoformat()
-                )
-                return df
-            except urllib.error.HTTPError:
-                print(f"Connection error with {url}. Retrying in {delay} seconds...")
-                time.sleep(delay)
-        raise ConnectionError(f"Failed to connect to {url} after {retries} retries")
+        response = retry_get(url, headers=yahoo_headers(), retries=retries, delay=delay)
+        data = StringIO(response.text)
+        df = pd.read_csv(data)
+        df["Date"] = pd.to_datetime(df["Date"]).dt.date.apply(lambda x: x.isoformat())
+        return df
 
     def append_frame(self, symbol: str):
         ticker = self.symbol_to_ticker(symbol)
--- systematic_trading/helpers.py
+++ systematic_trading/helpers.py
@@ -34,13 +34,32 @@ def nasdaq_headers():
     }
 
 
+def yahoo_headers():
+    return {
+        "authority": "query1.finance.yahoo.com",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "accept-language": "en-US,en;q=0.5",
+        "cache-control": "max-age=0",
+        "sec-ch-ua": '"Not/A)Brand";v="99", "Brave";v="115", "Chromium";v="115"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "sec-gpc": "1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    }
+
+
 def retry_get(
     url,
     headers={
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
     },
     retries=10,
-    delay=300,
+    delay=30,
     mode="default",
 ):
     if mode == "curl":
